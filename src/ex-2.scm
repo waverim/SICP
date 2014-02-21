7e9e98e6f728350be04f9c1c840720891616faa7
Waverim_Win <waverimax@gmail.com>
2014-02-21 16:14:10 +0800

    add from ex-2.3 to ex-2.10

--- src/ex-2.scm
+++ src/ex-2.scm
@@ -39,3 +39,86 @@
                  (x-point (end-segment s)))
         (list (average (y-point (start-segment s))
                        (y-point (end-segment s))))))
+
+; ex-2.3 rectangle, use the top-left point, width and height
+(define (rectangle width height start-point)
+  (cons width (cons height (list start-point))))
+
+(define (rectangle-circumference rectangle)
+  (double (+ (car rectangle)
+             (cadr rectangle))))
+
+(define (rectangle-area rectangle)
+  (* (car rectangle)
+     (cadr rectangle)))
+
+; construct (a b) of (exp 2 a)*(exp 3 b)
+; and to the opposite to cons a and b
+(define (cons-product a b)
+  (* (expt 2 a)
+     (expt 3 a)))
+
+(define (find-exp n base)
+  (define (iter try-exp)
+    (if (= 0 (remainder n (expt base try-exp)))
+        (iter (inc try-exp))
+        (- try-exp 1)))
+  (iter 1))
+
+(define (car-product x) (find-exp x 2))
+(define (cdr-product x) (find-exp x 3))
+
+; ex-2.6 Church numerals
+; zero = λf.λx.x
+(define zero
+  (lambda (f) 
+    (lambda (x) x)))
+
+; one = λf.λx.f x
+(define one
+  (lambda (f)
+    (lambda (x) (f x))))
+
+; two = λf.λx.f (f x)
+(define two 
+  (lambda (f)
+    (lambda (x) (f (f x)))))
+
+; plus = λm.λn.λf.λx.m f (n f x)
+(define (church-plus m n)
+  (lambda (f)
+    (lambda (x)
+      ((m f) ((n f) x)))))
+
+; Alyssa's problem: from ex-2.7 to ex-2.16 
+; ex-2.7 make-interval
+(define (make-interval a b) (cons a b))
+(define (upper-bound i) (max (car i) (cdr i)))
+(define (lower-bound i) (min (car i) (cdr i)))
+
+; ex-2.8 sub-interval
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+; ex-2.9 width
+(define (width-interval x y)
+  (halve (+ (- (upper-bound x) (lower-bound x))
+            (- (upper-bound x) (lower-bound x)))))
+
+; ex-2.10 improve div function
+(define (div-interval x y)
+  (if (or (= 0 (upper-bound y))
+          (= 0 (lower-bound y)))
+      (display "Error: Divide by zero")
+      (mul-interval x
+                    (make-interval (/ 1.0 (upper-bound y))
+                                   (/ 1.0 (lower-bound y))))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (lower-bound x) (lower-bound y)))
+        (p4 (* (lower-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))

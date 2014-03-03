534a19b59467335430a5bad535e39025698eb591
Waverim_Win <waverimax@gmail.com>
2014-03-03 14:41:36 +0800

    add ex-2.56 & ex-2.57

--- src/ex-2.scm
+++ src/ex-2.scm
@@ -470,3 +470,73 @@
          (and (equal-rec? (car l1) (car l2))
               (equal-rec? (cdr l1) (cdr l2))))
         (else #f)))
+
+; ex-2.56 exponentiation
+; code in the book
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2) (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (=number? exp num) (and (number? exp) (= exp num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (addend s) (cadr s))
+;(define (augend s) (caddr s))
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+
+; exponentiation
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base e) (cadr e))
+(define (exponent e) (caddr e))
+
+(define (make-exponentiation base exp)
+  (cond ((=number? base 1) 1)
+        ((=number? exp 0) 1)
+        ((=number? exp 1) base)
+        (else (list '** base exp))))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplier exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product 
+          (make-product (exponent exp)
+                        (make-exponentiation (base exp)
+                                             (if (number? (exponent exp))
+                                                 (- (exponent exp) 1)
+                                                 (' (- (exponent exp) 1)))))
+          (deriv (base exp) var)))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+; ex-2.57 sum or mulitiply a list
+(define (augend s)
+  (accumulate make-sum 0 (cddr s)))
+
+(define (mutiplicand p)
+  (accumulate make-product 1 (cddr p)))

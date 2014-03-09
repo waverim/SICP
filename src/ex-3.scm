d760d75174f85e733dd4efdeafee7bbc43805254
Waverim_Win <waverimax@gmail.com>
2014-03-09 15:50:00 +0800

    modify the random function in ex-3.5

--- src/ex-3.scm
+++ src/ex-3.scm
@@ -83,9 +83,7 @@
 (define random
   (let ((a 69069) (c 1) (m (expt 2 32)) (seed 19380110))
     (lambda new-seed
-      (if (pair? new-seed)
-          (set! seed (car new-seed))
-          (set! seed (modulo (+ (* seed a) c) m)))
+      (set! seed (modulo (+ (* seed a) c) m))
       (/ seed m))))
 
 (define (P x y)
